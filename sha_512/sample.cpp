7577a5d5957b363233a3828a4cade7f5554baeb1
Matěj Schrödl <msamot@seznam.cz>
2023-04-02 22:20:45 +0200

    Done

--- sha_512/sample.cpp
+++ sha_512/sample.cpp
@@ -25,22 +25,9 @@
 #define DIGEST_NAME "sha512"
 #define MESS_LENGTH 20
 
-bool checkStrings(const char* lhs, const char* rhs)
-{
-  if(strlen(lhs) != strlen(rhs)) return false;
-
-  for(size_t i = 0; i < strlen(lhs); i++)
-  {
-    if(lhs[i]!= rhs[i]) return false;
-  }
-
-  return true;
-}
-
 bool checkHash(int bits, unsigned char ct[EVP_MAX_MD_SIZE])
 {
 
-  // ct[i] by mělo být 8 bitů
   int toTransform = ceil(bits / 8.0);
 
   for (int i = 0; i < toTransform; i++)
@@ -49,7 +36,6 @@ bool checkHash(int bits, unsigned char ct[EVP_MAX_MD_SIZE])
     while (step >= 0)
     {
       // check if nth bit is zero, if not return false
-      // std::cout << (ct[i] >> step) << std::endl;
       if (!((ct[i] >> step) == 0))
         return false;
 
@@ -105,12 +91,14 @@ int findHash(int bits, char **message, char **hash)
     {
       std::cout << "RAND_bytes failed" << std::endl;
     }
+    // open_text[MESS_LENGTH] = '\0';
 
     EVP_DigestInit_ex(mdctx, md, NULL);
     EVP_DigestUpdate(mdctx, open_text, MESS_LENGTH);
     EVP_DigestFinal_ex(mdctx, md_value, &md_len);
 
     EVP_MD_CTX_destroy(mdctx);
+
   } while (!checkHash(bits, md_value));
 
   std::ostringstream oss;
@@ -123,7 +111,7 @@ int findHash(int bits, char **message, char **hash)
 
   // allocate memory for message output value
   *message = (char *)malloc(sizeof(char) * 2 * (MESS_LENGTH + 1));
-  OPENSSL_strlcpy(*message, message_const.c_str(), sizeof(char) * message_const.size()+1);
+  OPENSSL_strlcpy(*message, message_const.c_str(), sizeof(char) * message_const.size() + 1);
 
   // empty ostringstream buffer
   oss.str("");
@@ -135,10 +123,12 @@ int findHash(int bits, char **message, char **hash)
   std::string hash_const = oss.str();
   // allocate memory for hash output value
   *hash = (char *)malloc(sizeof(char) * 2 * (EVP_MAX_MD_SIZE + 1));
-  OPENSSL_strlcpy(*hash, hash_const.c_str(), sizeof(char) * hash_const.size()+1);
+  OPENSSL_strlcpy(*hash, hash_const.c_str(), sizeof(char) * hash_const.size() + 1);
 
   EVP_cleanup();
 
+  std::cout << *message << std::endl;
+  std::cout << *hash << std::endl;
   return 1;
 }
 
@@ -152,55 +142,55 @@ int findHashEx(int bits, char **message, char **hash, const char *hashFunction)
 
 int checkBits(int bits, char *hexString)
 {
-    //std::cout<<"checkHash got hash: "<<hexString<<std::endl;
-    int bigSteps = bits/4;
-    for(int i = 0; i< bigSteps; i++){ //kontrola celych bajtu hexStringu 
-        if(hexString[i] != '0') 
-            return 0;
-    }
 
-    //tady je potreba prekonvertovat 8bit char do "4bit" hexadecima
-    char testedChar = hexString[bigSteps];
-    if(hexString[bigSteps] >= '0' && hexString[bigSteps] <= '9')
-        testedChar -= '0';
-    else if(hexString[bigSteps] >= 'a' && hexString[bigSteps] <= 'f')
-        testedChar -= 'a'-10;
-    else if(hexString[bigSteps] >= 'A' && hexString[bigSteps] <= 'F') //tohle by snad ale nemelo byt potreba, hexadecimalni zapis mame v tomhle reseni jen lowercase
-        testedChar -= 'A'-10;
-    int smallSteps = bits -bigSteps*4;
-    for(int i = 0; i<smallSteps; i++){
-        //int bitmask = pow(2,3-i); // kontrola casti bajtu pomoci bitove masky
-        int bitmask = 1 << (3-i); // kontrola pomoci bitove masky... rychlejsi nez pow ig
-        if((bitmask & testedChar) != 0){
-            //std::cout<<"bitmask true on bit "<< i<< " of char "<<testedChar<<std::endl;
-            return 0;
-        }
-    }
-    return 1;
+  int bigSteps = bits / 4;
+  for (int i = 0; i < bigSteps; i++)
+  {
+    // check whole bytes
+    if (hexString[i] != '0')
+      return 0;
+  }
+
+  // converting hexadecimal character to binary
+  char testedChar = hexString[bigSteps];
+  if (hexString[bigSteps] >= '0' && hexString[bigSteps] <= '9')
+    testedChar -= '0';
+  else if (hexString[bigSteps] >= 'a' && hexString[bigSteps] <= 'f')
+    testedChar -= 'a' - 10;
+
+  int smallSteps = bits - bigSteps * 4;
+  for (int i = 0; i < smallSteps; i++)
+  {
+    //bitmask
+    int bitmask = 1 << (3 - i);
+    if ((bitmask & testedChar) != 0)
+      return 0;
+    
+  }
+  return 1;
 }
 
 int main(void)
 {
-    char *message, *hash;
+  char *message, *hash;
 
-    assert(findHash(0, &message, &hash) == 1);
-    assert(message && hash && checkBits(0, hash));
-    free(message);
-    free(hash);
-    assert(findHash(1, &message, &hash) == 1);
-    assert(message && hash && checkBits(1, hash));
-    free(message);
-    free(hash);
-    assert(findHash(2, &message, &hash) == 1);
-    assert(message && hash && checkBits(2, hash));
-    free(message);
-    free(hash);
-    assert(findHash(3, &message, &hash) == 1);
-    assert(message && hash && checkBits(3, hash));
-    free(message);
-    free(hash);
-    assert(findHash(-1, &message, &hash) == 0);
-    return EXIT_SUCCESS;
+  assert(findHash(0, &message, &hash) == 1);
+  assert(message && hash && checkBits(0, hash));
+  free(message);
+  free(hash);
+  assert(findHash(1, &message, &hash) == 1);
+  assert(message && hash && checkBits(1, hash));
+  free(message);
+  free(hash);
+  assert(findHash(2, &message, &hash) == 1);
+  assert(message && hash && checkBits(2, hash));
+  free(message);
+  free(hash);
+  assert(findHash(3, &message, &hash) == 1);
+  assert(message && hash && checkBits(3, hash));
+  free(message);
+  free(hash);
+  assert(findHash(-1, &message, &hash) == 0);
+  return EXIT_SUCCESS;
 }
 #endif /* __PROGTEST__ */
-
