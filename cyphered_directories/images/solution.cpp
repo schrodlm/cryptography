25d556bbc02a2c8d978dfecc0fa326710f86a086
Matěj Schrödl <schromat@fit.cvut.cz>
2023-04-05 15:28:40 +0200

    new

---
+++ cyphered_directories/images/solution.cpp
@@ -0,0 +1,118 @@
+#ifndef __PROGTEST__
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <climits>
+#include <cstdint>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <unistd.h>
+#include <string>
+#include <memory>
+#include <vector>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
+using namespace std;
+
+struct crypto_config
+{
+	const char * m_crypto_function;
+	std::unique_ptr<uint8_t[]> m_key;
+	std::unique_ptr<uint8_t[]> m_IV;
+	size_t m_key_len;
+	size_t m_IV_len;
+};
+
+#endif /* _PROGTEST_ */
+
+bool encrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config )
+{
+
+}
+
+bool decrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config )
+{
+
+}
+
+
+#ifndef __PROGTEST__
+
+bool compare_files ( const char * name1, const char * name2 )
+{
+
+}
+
+int main ( void )
+{
+	crypto_config config {nullptr, nullptr, nullptr, 0, 0};
+
+	// ECB mode
+	config.m_crypto_function = "AES-128-ECB";
+	config.m_key = std::make_unique<uint8_t[]>(16);
+ 	memset(config.m_key.get(), 0, 16);
+	config.m_key_len = 16;
+
+	assert( encrypt_data  ("homer-simpson.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "homer-simpson_enc_ecb.TGA") );
+
+	assert( decrypt_data  ("homer-simpson_enc_ecb.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "homer-simpson.TGA") );
+
+	assert( encrypt_data  ("UCM8.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "UCM8_enc_ecb.TGA") );
+
+	assert( decrypt_data  ("UCM8_enc_ecb.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "UCM8.TGA") );
+
+	assert( encrypt_data  ("image_1.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "ref_1_enc_ecb.TGA") );
+
+	assert( encrypt_data  ("image_2.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "ref_2_enc_ecb.TGA") );
+
+	assert( decrypt_data ("image_3_enc_ecb.TGA", "out_file.TGA", config)  &&
+		    compare_files("out_file.TGA", "ref_3_dec_ecb.TGA") );
+
+	assert( decrypt_data ("image_4_enc_ecb.TGA", "out_file.TGA", config)  &&
+		    compare_files("out_file.TGA", "ref_4_dec_ecb.TGA") );
+
+	// CBC mode
+	config.m_crypto_function = "AES-128-CBC";
+	config.m_IV = std::make_unique<uint8_t[]>(16);
+	config.m_IV_len = 16;
+	memset(config.m_IV.get(), 0, 16);
+
+	assert( encrypt_data  ("UCM8.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "UCM8_enc_cbc.TGA") );
+
+	assert( decrypt_data  ("UCM8_enc_cbc.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "UCM8.TGA") );
+
+	assert( encrypt_data  ("homer-simpson.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "homer-simpson_enc_cbc.TGA") );
+
+	assert( decrypt_data  ("homer-simpson_enc_cbc.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "homer-simpson.TGA") );
+
+	assert( encrypt_data  ("image_1.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "ref_5_enc_cbc.TGA") );
+
+	assert( encrypt_data  ("image_2.TGA", "out_file.TGA", config) &&
+			compare_files ("out_file.TGA", "ref_6_enc_cbc.TGA") );
+
+	assert( decrypt_data ("image_7_enc_cbc.TGA", "out_file.TGA", config)  &&
+		    compare_files("out_file.TGA", "ref_7_dec_cbc.TGA") );
+
+	assert( decrypt_data ("image_8_enc_cbc.TGA", "out_file.TGA", config)  &&
+		    compare_files("out_file.TGA", "ref_8_dec_cbc.TGA") );
+	return 0;
+}
+
+#endif /* _PROGTEST_ */
